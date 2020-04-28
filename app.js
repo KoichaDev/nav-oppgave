20476c65873731984aee077154a5bf4bcbeb01ea
Khoi Hoang <hello@koicha.dev>
2020-04-29 00:35:22 +0200

    :sparkles: Added jest and calculation for birth

--- app.js
+++ app.js
@@ -1,141 +1,94 @@
+const numbers = (digit) => (digit);
+
+const dNr = (digit) => {
+    const toString = numbers(digit).toString();
+    return toString.substring(0, 2)
+}
+const mNr = (digit) => {
+    const toString = numbers(digit).toString();
+    return toString.substring(2, 4)
+}
+const yNr = (digit) => {
+    const toString = numbers(digit).toString();
+    return toString.substring(4, 6)
+}
+
+const iNr = (digit) => {
+    const toString = numbers(digit).toString();
+    return toString.substring(6, 9)
+}
+
 const validate = (digit) => {
-    const elevenDigits = new RegExp('^[0-9]{11}$')
-    const toString = number.toString();
-    if (elevenDigits.test(toString)) return number;
+    const elevenDigits = new RegExp('^[0-9]{11}$');
+    if (!elevenDigits.test(parseInt(digit))) {
+        return {
+            msg: 'Invalid',
+            reason: 'Must be eleven digits'
+        }
+    }
+    return parseInt(digit);
 };
 
-
-/*
-const validateBirth = (arr) => arr.map(number => {
-    const elevenDigits = new RegExp('^[0-9]{11}$')
-    const toString = number.toString();
-    if (elevenDigits.test(toString)) return number;
-});
-
-const getData = (array) => {
-}
-
-const dNr = (number, arr = validateBirth(mockData)) => {
-    const data = arr.map(digit => {
-        let position = number;
-        const toString = digit.toString();
-        const array = toString.substring(0, 2).split('');
-        return array[position]
-    })
-    return data.forEach(digit => digit)
-
-}
-
-const mNr = (number, arr = validateBirth(mockData)) => {
-    return arr.map(digit => {
-        let position = number;
-        const toString = digit.toString();
-        const array = toString.substring(2, 4).split('');
-        return array[position]
-    })
-}
-
-const yNr = (number, arr = validateBirth(mockData)) => {
-    return arr.map(digit => {
-        let position = number;
-        const toString = digit.toString();
-        const array = toString.substring(4, 6).split('');
-        return array[position]
-    })
-}
-
-const iNr = (number, arr = validateBirth(mockData)) => {
-    return arr.map(digit => {
-        let position = number;
-        const toString = digit.toString();
-        const array = toString.substring(6, 9).split('');
-        return array[position]
-    })
-}
-
-const checkSum = (d1, d2, m1, m2, y1, y2, i1, i2, i3) => {
-    d1 = dNr(0);
-    d2 = dNr(1);
-    m1 = mNr(0);
-    m2 = mNr(1);
-    y1 = yNr(0);
-    y2 = yNr(1);
-    i1 = iNr(0);
-    i2 = iNr(1);
-    i3 = iNr(2);
-
-    let k1 = 11 - ((3 * d1 + 7 * d2 + 6 * m1 + 1 * m2 + 8 * y1 + 9 * y2 + 4 * i1 + 5 * i2 + 2 * i3) % 11);
-    let k2 = 11 - ((5 * d1 + 4 * d2 + 3 * m1 + 2 * m2 + 7 * y1 + 6 * y2 + 5 * i1 + 4 * i2 + 3 * i3 + 2 * k1) % 11);
-}
-
-
-
-
-
-
-
-
-
-
-
-let d1, d2, m1, m2, y1, y2, i1, i2, i3;
-for (const numbers of mockData) {
-    const toString = numbers.toString();
-    const split = toString.split('').map(Number);
-
-    d1 = split[0];
-    d2 = split[1];
-    m1 = split[2];
-    m2 = split[3];
-    y1 = split[4];
-    y2 = split[5];
-    i1 = split[6];
-    i2 = split[7];
-    i3 = split[8];
-
-    const individNr = parseInt([i1, i2, i3].join(''));
-
-    let k1 = 11 - ((3 * d1 + 7 * d2 + 6 * m1 + 1 * m2 + 8 * y1 + 9 * y2 + 4 * i1 + 5 * i2 + 2 * i3) % 11);
-    let k2 = 11 - ((5 * d1 + 4 * d2 + 3 * m1 + 2 * m2 + 7 * y1 + 6 * y2 + 5 * i1 + 4 * i2 + 3 * i3 + 2 * k1) % 11);
-
-    k1 === 11 ? k1 = 0 : k1;
-
-    if (k2 === 0) {
-        k2 = 0;
-    } else if (k2 === 11) {
-        k2 = 11 - k2
+const arrayEquals = (left, right) => {
+    const { isArray } = Array;
+    if (!isArray(left) || !isArray(right)) {
+        return false;
     }
-    //console.log(k1, k2);
 
-    /*
-        //  000–499 omfatter personer født i perioden 1900–1999.
-        if (individNr < 499) {
-            console.log('1900+: 000-499: ', individNr, true);
-        } else {
-            console.log('1900+: - 000-499: ', individNr, false);
+    const { length } = left;
+    if (length !== right.length) {
+        return false;
+    }
+
+    for (let i = 0; i < length; ++i) {
+        if (left[i] !== right[i]) {
+            return false;
         }
+    }
 
-        //  500–749 omfatter personer født i perioden 1854–1899.
-        if (499 < individNr || individNr > 749) {
-            console.log('+1899: - 500–749: ', individNr, true);
-        } else {
-            console.log('+1899: - 500–749: ', individNr, false);
-        }
+    return true;
+}
 
-        //  500–999 omfatter personer født i perioden 2000–2039.
-        if (500 < individNr || individNr > 999) {
-            console.log('2000-2039: - 500-999: ', individNr, true);
-        } else {
-            console.log('2000-2039: - 500-999: ', individNr, false);
-        }
+const checkSum = (digit) => {
+    // 1. Array for input of birth digit
+    // 2. Array for checkSum calculation
+    const input = [validate(digit)];
+    const checkSum = [];
 
-        //  900–999 omfatter personer født i perioden 1940–1999.
-        if (900 < individNr || individNr > 999) {
-            console.log('1940–1999 - 900–999: ', individNr, true);
-        } else {
-            console.log('1940–1999 - 900–999: ', individNr, false);
-        }
-     */
+    // Manipulating input birth digit 
+    const toString = input.toString();
+    const split = toString.split('');
+    const toNumberArr = Array.from(split, Number)
+
+    const d1 = parseInt(dNr(digit)[0])
+    const d2 = parseInt(dNr(digit)[1])
+    const m1 = parseInt(mNr(digit)[0])
+    const m2 = parseInt(mNr(digit)[1])
+    const y1 = parseInt(yNr(digit)[0])
+    const y2 = parseInt(yNr(digit)[1])
+    const i1 = parseInt(iNr(digit)[0])
+    const i2 = parseInt(iNr(digit)[1])
+    const i3 = parseInt(iNr(digit)[2])
+
+    let k1 = 11 - ((3 * d1 + 7 * d2 + 6 * m1 + 1 * m2 + 8 * y1 + 9 * y2 + 4 * i1 + 5 * i2 + 2 * i3) % 11);
+    let k2 = 11 - ((5 * d1 + 4 * d2 + 3 * m1 + 2 * m2 + 7 * y1 + 6 * y2 + 5 * i1 + 4 * i2 + 3 * i3 + 2 * k1) % 11);
+
+    if (k1 === 11) k1 = 0
+    if (k2 === 11) k2 = 0
+
+    checkSum.unshift(d1, d2, m1, m2, y1, y2, i1, i2, i3, k1, k2);
+
+    return (arrayEquals(checkSum, toNumberArr))
+}
+
+console.log(checkSum(18081888015))
 
 
-//}
+
+
+
+
+module.exports = numbers;
+module.exports = dNr;
+module.exports = mNr;
+module.exports = yNr;
--- app.test.js
+++ app.test.js
@@ -1,3 +1,32 @@
-const validate = require('./app.test.js');
+const numbers = require('./app.js');
+const dNr = require('./app.js');
+const mNr = require('./app.js');
+const yNr = require('./app.js');
 
-test 
+test('expect datatype number', () => {
+    expect(typeof parseInt(numbers(25108338752))).toBe('number');
+});
+/*
+test('expect to reject if less than 11 digits', () => {
+    expect(numbers(12367).toString()).toHaveLength(11);
+})
+
+
+test('expect to reject if more than 11 digits', () => {
+    expect(numbers(211215108338752).toString()).not.toHaveLength(11);
+})
+
+test('expect day/month/year digit to be 2 numbers', () => {
+    expect(typeof dNr(25108338752)).toBe('number');
+    expect(typeof mNr(25108338752)).toBe('number');
+    expect(typeof yNr(25108338752)).toBe('number');
+}) */
+
+
+
+
+
+
+/* test('expect datatype number', () => {
+    expect(typeof '1726371928').toBe('string')
+}) */
